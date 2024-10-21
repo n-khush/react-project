909190d26fcc3703594b2dc55375bacf5cfae50f
Naseem Khan <khushnasimkhan8882@gmail.com>
2024-10-21 17:42:40 +0530

    Using react with CDN

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-const parent = React.createElement("div",{id:"parent"},
-[ 
-    React.createElement("div",{id:"child1"},
-    [
-        React.createElement("h1",{id:"c1c1"},"I'm and H1 tag child 1 of child1 of parent"),
-        React.createElement("h2",{id:"c1c2"},"I'm and H2 tag child 2 of child1 of parent")
-    ]),
-    React.createElement("div",{id:"child2"},
-    [
-        React.createElement("h1",{id:"c2c1"},"I'm and H1 tag child 1 of child2 of parent"),
-        React.createElement("h2",{id:"c2c2"},"I'm and H2 tag child 2 of child2 of parent")
-    ])
-])
+const child1 = React.createElement("h1",{id:"child11"},"This is first of first children")
+const child2 = React.createElement("h2",{id:"child12"},"This is second  of first children")
+const child3 = React.createElement("h1",{id:"child21"},"This is first of second children")
+const child4 = React.createElement("h2",{id:"child22"},"This is second of second children")
 
-const heading = React.createElement(
-  "h1", 
-  { id: "heading" },
-  "Hello World from React! "
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},[child1,child2]),React.createElement("div",{id:"child1"},[child3,child4])])
+const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(parent);
